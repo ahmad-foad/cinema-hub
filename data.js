7cce086abfdf8d9f4332c6b541117a6cf0e5b34f
ahmed-foad <etec02m230476@epu.edu.iq>
2025-02-27 17:37:14 +0300

    Add Series

--- data.js
+++ data.js
@@ -206,6 +206,78 @@ export const series = [
         "title": "Criminality Series: Rogue Justice",
         "image": "https://i.ytimg.com/vi/ymSKZt-hNlY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLARdMOrNm4Zeqwd-nXOu-VarJSHAA",
         "description": "A former cop turned vigilante takes justice into his own hands in a city where the law has failed."
+    },
+    {
+        "id": 11,
+        "title": "24",
+        "image": "https://w0.peakpx.com/wallpaper/978/682/HD-wallpaper-24-live-another-day-cool-tv-series-entertainment-fun-24-thumbnail.jpg",
+        "description": "Counterterrorism agent Jack Bauer races against time to stop deadly threats, uncover conspiracies, and save lives, all in real-time."
+    },
+    {
+        "id": 12,
+        "title": "Prison Break",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZZqCxEsf9q8MDL907N94a3HHa4DSLdZZrWA&s",
+        "description": "A man intentionally gets imprisoned to break his wrongly accused brother out, leading to a thrilling escape and high-stakes conspiracies."
+    },
+    {
+        "id": 13,
+        "title": "The Punisher",
+        "image": "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p14716114_b_v8_af.jpg",
+        "description": "A former Marine-turned-vigilante, Frank Castle, seeks revenge on those responsible for his family’s murder while uncovering deeper conspiracies."
+    },
+    {
+        "id": 14,
+        "title": "Arrow",
+        "image": "https://m.media-amazon.com/images/M/MV5BNjRlNjNlY2YtYzQxNS00ZTUzLTkwMTQtMjM0YjZlOWQwZmFkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        "description": "Billionaire playboy Oliver Queen returns from a deadly island to fight crime in his city, using his archery skills and a secret identity."
+    },
+    {
+        "id": 15,
+        "title": "Banshee",
+        "image": "https://ih1.redbubble.net/image.5103823533.0253/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg",
+        "description": "An ex-con assumes the identity of a murdered sheriff and enforces his own brand of justice in a small, violent town."
+    },
+    {
+        "id": 16,
+        "title": "Reacher",
+        "image": "https://m.media-amazon.com/images/I/71LGGcQHzqL._AC_UF894,1000_QL80_.jpg",
+        "description": "A former military investigator, Jack Reacher, gets caught up in deadly conspiracies while uncovering the truth and serving justice."
+    },
+    {
+        "id": 17,
+        "title": "Black Mirror",
+        "image": "https://static.next-episode.net/tv-shows-images/huge/black-mirror.jpg",
+        "description": "A dystopian anthology exploring the dark side of technology and its impact on society, morality, and human behavior."
+    },
+    {
+        "id": 18,
+        "title": "Fringe",
+        "image": "https://w0.peakpx.com/wallpaper/665/253/HD-wallpaper-fringe-american-tv-series-thumbnail.jpg",
+        "description": "A team of scientists and FBI agents investigate mysterious and unexplained phenomena linked to fringe science."
+    },
+    {
+        "id": 19,
+        "title": "The Expanse",
+        "image": "https://cdn1.epicgames.com/offer/8b2f4c9ee4e74d05b801565de8fd4500/EGS_TheExpanseATelltaleSeries_DeckNine_S1_2560x1440-5708ef472e393756d0144065d2c77422?resize=1&w=480&h=270&quality=medium",
+        "description": "A gripping space opera where political tensions, interplanetary conflicts, and scientific discoveries shape the future of humanity."
+    },
+    {
+        "id": 20,
+        "title": "Westworld",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJZAtQJO9q-b2Zy1R8Z-YjCG76ila7It-NUA&s",
+        "description": "A futuristic theme park filled with AI hosts sparks an existential crisis when the line between human and machine blurs."
+    },
+    {
+        "id": 21,
+        "title": "Person of Interest",
+        "image": "https://resizing.flixster.com/QUfdsJpdYumP3vbRnFfNlzmgKL8=/fit-in/352x330/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8808512_b_h9_ab.jpg",
+        "description": "A billionaire programmer creates an AI surveillance system that predicts crimes before they happen, leading to a high-stakes game of justice."
+    },
+    {
+        "id": 22,
+        "title": "Devs",
+        "image": "https://resizing.flixster.com/5ECXIaMPa_MzNxZhsj9ycLPEWtI=/fit-in/352x330/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p17334711_b_v10_aa.jpg",
+        "description": "A software engineer investigates a secretive tech company’s quantum computing division, uncovering reality-shattering discoveries."
     }
 
 ];
